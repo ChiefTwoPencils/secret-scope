797a34230eb3251c0b5d74bd71ed3233c9d066ae
Ron <ron.lewis@gmail.com>
2016-04-22 09:08:01 -0700

    Refactored a bit so each test has its own new collider. Some tests fail, will check that out later.

--- src/test/java/collidascope/ColliderTest.java
+++ src/test/java/collidascope/ColliderTest.java
@@ -1,17 +1,12 @@
 package collidascope;
 
-import collidascope.collidahandla.Handler;
-import collidascope.collidadetecta.Detector;
-import collidascope.collidatracka.Tracker;
 import examples.Enemy;
 import examples.Friend;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
@@ -26,43 +21,46 @@ public class ColliderTest {
     private ICollider a, b;
     private Collider collider;
     private Collision collision;
-    private Handler handler;
-    private Detector detector;
-    private Tracker tracker;
     private List<String> keys;
     private List<BiConsumer<ICollider, ICollider>> consumers;
     private List<BiFunction<ICollider, ICollider, Boolean>> functions;
+    private Map<String, BiConsumer<ICollider, ICollider>> consumerMap;
+    private Map<String, BiFunction<ICollider, ICollider, Boolean>> functionMap;
     private static int expected = 0;
     private static int actual = 0;
 
-    private final int MANY = 2;
-
     @Before
     public void setUp() throws Exception {
-        detector = new Detector();
-        handler = new Handler();
-        tracker = new Tracker();
         keys = new ArrayList<>();
         consumers = new ArrayList<>();
         functions = new ArrayList<>();
+        consumerMap = new HashMap<>();
+        functionMap = new HashMap<>();
         a = new Friend();
         b = new Enemy();
         collision = new Collision(a, b);
         keys.add(a.getCollisionKey() + b.getCollisionKey());
         consumers.add(getColliderConsumer());
         functions.add(getRandomFunction());
-        tracker.track(collision);
-        collider = new Collider(keys, functions,
-                                keys, consumers);
+        for (int i = 0; i < keys.size(); i++) {
+            consumerMap.put(keys.get(i), consumers.get(i));
+            functionMap.put(keys.get(i), functions.get(i));
+        }
     }
 
     @Test
     public void testDetectedCollision() throws Exception {
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
         Assert.assertSame(functions.get(0).apply(a, b), collider.detectedCollision(a, b));
     }
 
     @Test
     public void testTrackCollision() throws Exception {
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
         collider.trackCollision(collision);
         expected = 1;
         actual = 0;
@@ -76,26 +74,46 @@ public class ColliderTest {
 
     @Test
     public void testHandleCollision() throws Exception {
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
         expected = 1;
         actual = 0;
         collider.handleCollision(a, b);
         Assert.assertEquals(expected, actual);
-
     }
 
     @Test
     public void testHandleCollisions() throws Exception {
-
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
+        expected = 1;
+        actual = 0;
+        collider.handleCollisions();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testAddHandlers() throws Exception {
-
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
+        expected = 1;
+        actual = 0;
+        collider.handleCollisions();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testAddHandlers1() throws Exception {
-
+        collider = new Collider();
+        collider.addDetectors(keys, functions);
+        collider.addHandlers(keys, consumers);
+        expected = 1;
+        actual = 0;
+        collider.handleCollisions();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
